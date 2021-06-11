d92bdfd6c4471a8abe6088be67c4a9b0405e65ef
kumarishalini6 <kumarishalu1206@gmail.com>
2021-06-11 10:23:55 -0800

    framework

--- src/Data/Frameworks.js
+++ src/Data/Frameworks.js
@@ -48,6 +48,54 @@ let frameworks = [
         "https://github.com/angular/angular-seed "
       ]
     }
+  },{
+    title: "ReactJS",
+    path: "/frameworks/reactjs",
+    image: "",
+    documentation: "https://reactjs.org/docs",
+    content: {
+      youtube: [
+        "https://www.youtube.com/watch?v=RGKi6LSPDLU ",
+        "https://www.youtube.com/watch?v=QFaFIcGhPoM&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3 ",
+        "https://www.youtube.com/watch?v=fSp2C7QPH8M ",
+        "https://www.youtube.com/watch?v=CI2kX2EFhWc ",
+        "https://www.youtube.com/watch?v=GiyL4KFRNBA ",
+        "https://www.youtube.com/watch?v=w7ejDZ8SWv8 "
+      ],
+      websites: [
+        "https://reactjs.org/ ",
+        "https://clockwise.software/react-development-company/ ",
+        "https://stfalcon.com/en/blog/post/large-websites-using-AngularJS ",
+        "https://www.creative-tim.com/blog/angularjs/angular-js-75-examples-websites-applications-experiments/ ",
+        "https://wiredelta.com/10-most-popular-angular-websites-of-2020/ ",
+        "https://www.zarantech.com/blog/top-15-websites-and-applications-built-with-angularjs/ "
+      ],
+      courses: [
+        "https://www.udemy.com/course/learn-angularjs/ ",
+        "https://www.udemy.com/course/angularjs-for-beginners-udemy/ ",
+        "https://www.coursera.org/learn/single-page-web-apps-with-angularjs ",
+        "https://www.coursera.org/specializations/full-stack-mobile-app-development ",
+        "https://www.coursera.org/learn/angular ",
+        "https://www.udemy.com/course/maruti-angularjs/ "
+      ],
+
+      tips: [
+        "https://www.keycdn.com/blog/angular-performance ",
+        "https://www.techbeamers.com/angularjs-tutorial-beginners-essential-tips-tricks/ ",
+        "https://www.toptal.com/angular-js/tips-and-practices ",
+        "https://www.kiuwan.com/14-tips-for-developing-angularjs-applications/ ",
+        "https://eng.localytics.com/tips-and-tricks-for-debugging-unfamiliar-angularjs-code/ ",
+        "https://dzone.com/articles/10-tips-for-using-angularjs "
+      ],
+      github: [
+        "https://github.com/angular/angular.js/ ",
+        "https://github.com/angular/angularjs.org ",
+        "https://github.com/gothinkster/angularjs-realworld-example-app ",
+        "https://github.com/PatrickJS/awesome-angular ",
+        "https://github.com/DanWahlin/CustomerManager ",
+        "https://github.com/angular/angular-seed "
+      ]
+    }
   }
 ];
 

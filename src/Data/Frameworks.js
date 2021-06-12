ebf6fe6118dc16d193734bc3be6d7c16cac5c6e6
kumarishalini6 <kumarishalu1206@gmail.com>
2021-06-12 10:31:19 -0800

    added more frameworks

--- src/Data/Frameworks.js
+++ src/Data/Frameworks.js
@@ -114,34 +114,82 @@ let frameworks = [
         "https://nodejs.org/en/ ",
         "https://clockwise.software/node-js-development-company/ ",
         "https://www.coursera.org/learn/server-side-nodejs?ranMID=40328&ranEAID=vedj0cWlu2Y&ranSiteID=vedj0cWlu2Y-p6s.CfBELfyLJl0sFWKWKA&siteID=vedj0cWlu2Y-p6s.Cf",
-        "https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/",
-        "https://www.udemy.com/course/nodejs-the-complete-guide/",
-        "https://www.udemy.com/course/the-complete-nodejs-developer-course-2/"
+        "https://www.crio.do/",
+        "https://www.pluralsight.com/courses/nodejs-getting-started?aid=7010a000002BWq6AAG&promo=&utm_source=non_branded&utm_medium=digital_paid_search_google&utm_campaign=IN_Dynamic&utm_content=&cq_cmp=846648680&gclid=Cj0KCQjwk4yGBhDQARIsACGfAetjSzh_UQ8q4O_C8uwYJitWWuRIIyXed26NIaLk03ukDGRQGqYfaowaAhrqEALw_wcB",
+        ""
       ],
       courses: [
-        "https://www.udemy.com/course/react-the-complete-guide-incl-redux/",
-        "https://www.udemy.com/topic/react-hooks/ ",
-        "https://www.coursera.org/learn/front-end-react ",
-        "https://www.masaischool.com/courses/full_stack",
-        "https://blog.glitch.com/post/react-starter-kit ",
-        "https://www.codecademy.com/learn/react-101 "
+        "https://www.udemy.com/course/nodejs-the-complete-guide/",
+        "https://www.linkedin.com/learning/paths/become-a-node-js-developer?src=go-pa&veh=sem_src.go-pa_c.LLS-C_APAC_All_IN_Search_Google-NB_DR-PRS_Broad_Course-DSA_All_English_Core_pkw._pmt.b_pcrid.473973938308_pdv.c_plc._trgid.aud-1016811499848:dsa-922770129747_net.s_learning&trk=sem_src.go-pa_c.LLS-C_APAC_All_IN_Search_Google-NB_DR-PRS_Broad_Course-DSA_All_English_Core_pkw._pmt.b_pcrid.473973938308_pdv.c_plc._trgid.aud-1016811499848:dsa-922770129747_net.s_learning&mcid=6626616148786065459&cname=&camid=11414361728&asid=114147496720&targetid=aud-1016811499848:dsa-922770129747&crid=473973938308&placement=&dev=c&ends=1&gclid=Cj0KCQjwk4yGBhDQARIsACGfAeuuSxufmgiyzhSMDYKlkV2Ri06j02htUq6LDzGz-UTgoCvo055rluYaAvvtEALw_wcB&gclsrc=aw.ds",
+        "https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/",
+        "https://www.crio.do/backend-developer-track",
+        "https://www.pluralsight.com/courses/nodejs-getting-started?aid=7010a000002BWq6AAG&promo=&utm_source=non_branded&utm_medium=digital_paid_search_google&utm_campaign=IN_Dynamic&utm_content=&cq_cmp=846648680&gclid=Cj0KCQjwk4yGBhDQARIsACGfAetjSzh_UQ8q4O_C8uwYJitWWuRIIyXed26NIaLk03ukDGRQGqYfaowaAhrqEALw_wcB",
+        "https://www.udemy.com/course/the-complete-nodejs-developer-course-2/"
       ],
 
       tips: [
-        "https://www.creativebloq.com/news/5-expert-reactjs-tips-that-you-need-to-know-today ",
-        "https://www.freecodecamp.org/news/the-beginners-collection-of-powerful-tips-and-tricks-for-react-f2e3833c6f12/ ",
-        "http://react.tips/ ",
-        "https://www.geeksforgeeks.org/how-to-learn-reactjs-in-2021/ ",
-        "https://www.infoworld.com/article/3616579/10-tips-for-tuning-react-ui-performance.html ",
-        "https://javascript.plainenglish.io/8-react-tips-tricks-1f1a429d7da0 "
+        "https://www.mobileappdaily.com/nodejs-development-tips#:~:text=Node.js%20Tips%20and%20Tricks%20for%20JavaScript%20Developers%201,almost%20a%20standard%20now.%20...%20More%20items...%20",
+        "https://medium.com/@faisalabid/7-tips-for-a-node-js-padawan-e7c0b0e5ce3c",
+        "https://www.section.io/engineering-education/ten-tips-for-working-with-nodejs/",
+        "https://blog.techmagic.co/10-best-ways-to-fasten-your-node-js-development-process/",
+        "https://developersjournal.in/top-10-nodejs-tips-developers/ ",
+        "https://docs.pivotal.io/application-service/2-8/buildpacks/node/node-tips.html"
       ],
       github: [
-        "https://github.com/facebook/react ",
-        "https://github.com/K-G-PRAJWAL/ReactJS-Projects ",
-        "https://github.com/Priyank-Ahuja/Uber-Frontend-ReactJS- ",
-        "https://github.com/kritika27/markdown-react ",
-        "https://github.com/ajayns/react-projects ",
-        "https://github.com/DibasDauliya/reactJS-projects "
+        "https://github.com/nodejs/node",
+        "https://github.com/nodejs/node-addon-api",
+        "https://github.com/nodejs/Release",
+        "https://github.com/nodejs/node-gyp",
+        "https://github.com/nodejs/community-committee",
+        "https://github.com/nodejs/help"
+      ]
+    }
+  },{
+    title: "Django",
+    path: "/frameworks/Django",
+    image: "https://icts.io/wp-content/uploads/2020/04/django_final.png",
+    documentation: "https://docs.djangoproject.com/en/3.2/",
+    content: {
+      youtube: [
+        "https://www.youtube.com/watch?v=OTmQOjsl0eg",
+        "https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p ",
+        "https://www.youtube.com/watch?v=sccLfQ4_u10&list=PLbGui_ZYuhigchy8DTw4pX4duTTpvqlh6 ",
+        "https://www.youtube.com/watch?v=HRLIEgwYSHc",
+        "https://www.youtube.com/watch?v=qgGIqRFvFFk&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK",
+        "https://www.youtube.com/watch?v=LoJ67Ik6ynM"
+      ],
+      websites: [
+        "https://docs.djangoproject.com/en/3.2/ ",
+        "https://en.wikipedia.org/wiki/Django_(web_framework)",
+        "https://onlinecoursetutorials.com/courses/best-django-courses-online-with-certifications/#seven",
+        "https://www.tangowithdjango.com/",
+        "https://www.feldroy.com/pages/django-tutorials",
+        "https://www.coursary.com/search?fromfld=lpads&q=django&act=1623472358&refsrc=ms"
+      ],
+      courses: [
+        "https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp/?matchtype=b&msclkid=0b81e8c0171413fbb84d880421be07ff&utm_campaign=BG-LongTail_la.EN_cc.INDIA&utm_content=deal4584&utm_medium=udemyads&utm_source=bing&utm_term=_._ag_1216060273134072_._ad__._kw_%2BDjango+%2BTraining_._de_c_._dm__._pl__._ti_kwd-76004079531314%3Aloc-90_._li_1666_._pd__._",
+        "https://www.linkedin.com/learning/topics/django?src=aff-lilpar&veh=aff_src.aff-lilpar_c.partners_pkw.10078_plc.Skimbit%20Ltd._pcrid.449670_learning&trk=aff_src.aff-lilpar_c.partners_pkw.10078_plc.Skimbit%20Ltd._pcrid.449670_learning&clickid=TGDTdaQOsxyLUeIwUx0Mo3QsUkBwJw0sDz%3AOxQ0&irgwc=1",
+        "https://www.skillshare.com/site/join?via=search-layout-grid&clickid=THb2z9QOsxyLRILwUx0Mo3QMUkBwJwyQDz%3AOxQ0&irgwc=1&utm_content=4650&utm_term=Skimbit+Ltd.&utm_campaign=10078&affiliateRef=6595003&utm_medium=affiliate-referral&utm_source=IR&t=Try-Django-Learn-and-Master-the-Python-Programming-Web-Framework&sku=1251826101",
+        "https://www.pluralsight.com/courses/django-angularjs-web-development?clickid=TGDTdaQOsxyLUeIwUx0Mo3QsUkBwJwRcDz%3AOxQ0&irgwc=1&mpid=10078&aid=7010a000001xAKZAA2&utm_medium=digital_affiliate&utm_campaign=10078&utm_source=impactradius",
+        "https://www.tangowithdjango.com/",
+        "https://www.feldroy.com/pages/django-tutorials"
+      ],
+
+      tips: [
+        "https://medium.com/@matlapeng/django-tips-and-tricks-part-1-3517ad9a9257 ",
+        "https://code.tutsplus.com/tutorials/10-insanely-useful-django-tips--net-974 ",
+        "https://letslearnabout.net/blog/16-tips-for-python-and-django-beginners/",
+        "https://www.bedjango.com/blog/tips-and-tricks-improved-django-security/ ",
+        "https://www.ericholscher.com/blog/2008/oct/5/django-tips/ ",
+        "https://skytechgeek.com/2019/07/tips-learning-django/ "
+      ],
+      github: [
+        "https://github.com/django/django ",
+        "https://github.com/django/django/tree/main/django",
+        "https://github.com/MicroPyramid/opensource-job-portal ",
+        "https://github.com/gothinkster/django-realworld-example-app",
+        "https://github.com/django-oscar/django-oscar",
+        " https://github.com/wagtail/wagtail"
       ]
     }
   }

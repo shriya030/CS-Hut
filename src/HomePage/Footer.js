1df976ceefed09d790b9b34c2c3ab35432025760
Rishabh Rathore <77366500+rishabhrathore055@users.noreply.github.com>
2021-06-18 21:27:17 +0530

    Update Footer.js

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -1,21 +1,4 @@
 import React from "react";
-import "./Footer.css";
-function Footer() {
-  return (
-    <div>
-      <div className="footer">
-        <ul className="list">
-          <li className="item">© 2021 All rights reserved</li>
-          <li className="item">Efforts By Shriya</li>
-          <li className="item">Send US Feedback</li>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export default Footer;
-import React from "react";
 import {Link} from 'react-router-dom'
 import "./Footer.css";
 function Footer() {

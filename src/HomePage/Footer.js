6a3ba4e8ebf7c24fb471067153b104c872270907
Shaishav Rastogi <rastogishaishav@gmail.com>
2021-06-18 10:49:00 +0530

    Automatic Updation of Footer Copyright year

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
+  const currentyear = new Date().getFullYear();
   return (
     <div>
       <div className="footer">
         <ul className="list">
-          <li className="item">© 2021 All rights reserved</li>
+          <li className="item">© {currentyear} All rights reserved</li>
           <li className="item">Efforts By Shriya</li>
           <li className="item">Send US Feedback</li>
         </ul>

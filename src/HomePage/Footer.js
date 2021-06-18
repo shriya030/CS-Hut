82e83ab634b0adb43e959e291ad696e9627f90f9
Rishabh Rathore <77366500+rishabhrathore055@users.noreply.github.com>
2021-06-18 14:00:23 +0530

    Link Feedback Form

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -15,3 +15,23 @@ function Footer() {
 }
 
 export default Footer;
+import React from "react";
+import {Link} from 'react-router-dom'
+import "./Footer.css";
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <ul className="list">
+          <li className="item">© 2021 All rights reserved</li>
+          <li className="item">Efforts By Shriya</li>
+          <Link style={{"width":"100%", textDecoration: 'none',"color":"white","margin-top":"30px","margin-left":"50px","border-bottom":"1px solid white" }} to="/contactus">
+          <li>Send US Feedback</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;

dbbe005a1fd7ec3c14f519e64ebb159f92a466f1
Samiksha sandip sankar <samikshasankar2000@gmail.com>
2021-06-19 22:50:05 +0530

    update style of tags card

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -6,38 +6,35 @@ function Tags() {
   return (
     <div className="tags">
       <div className="headings">Tags</div>
-      <div class="cards">
-        <Link to="/languages">
-          <div class="cards__item">
-            <div class="card">
-              <div class="card__content">
-                <div class="card__title">Programming Languages</div>
-                <p class="card__text">C, C++, Java, Python,....</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/frameworks">
-          <div class="cards__item">
-            <div class="card">
-              <div class="card__content">
-                <div class="card__title">Frameworks/Libraries</div>
-                <p class="card__text">AngularJS, ReactJS, Pygame,....</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/databases">
-          <div class="cards__item">
-            <div class="card">
-              <div class="card__content">
-                <div class="card__title">Databases</div>
-                <p class="card__text">SQL, MongoDB, ....</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <div class="wrapper">
+<div class="card">
+	<h3 class="card___title">PROGRAMMING LANGUAGES</h3>
+		<div class="card__content">
+         <div class="card__text">C</div>
+         <div class="card__text">C++</div>
+         <div class="card__text">Java</div> 
+         <div class="card__text">Python</div>
+        </div>
+		<Link to="/languages"><button class="card-btn">Know More</button></Link>
+	</div>
+	<div class="card">
+		<h3 class="card___title">FRAMEWORKS/LIBRARIES</h3>
+        <div class="card__content">
+         <div class="card__text">Angular JS</div>
+         <div class="card__text">ReactJS</div>
+         <div class="card__text">Pygame</div> 
+        </div>
+         <Link to="/frameworks"><button class="card-btn">Know More</button></Link>
+	</div>
+	<div class="card">
+		<h3 class="card___title">DATABASES</h3>
+      <div class="card__content">
+         <div class="card__text">SQL</div>
+         <div class="card__text">MongoDB</div> 
+        </div>
+         <Link to="/databases"><button class="card-btn">Know More</button></Link>
+	</div>
+</div>
     </div>
   );
 }
